20dbd22a120882ede858232c38be9e30b58d3e52
Espe-On <eljoe0@googlemail.com>
2020-02-03 15:51:37 +0000

    switched to string building method using if statements

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -6,7 +6,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("We are going to play Fizz Buzz!");
+            /* Console.WriteLine("We are going to play Fizz Buzz!");
             Console.WriteLine("What Number is Fizz?");
             int fizzNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("What Number is Buzz?");
@@ -16,29 +16,37 @@ namespace FizzBuzz
             Console.WriteLine("Fizz is " + fizzNumber + ", Buzz is " + buzzNumber);
             Console.WriteLine("We are going up to " + countTotal);
             Console.WriteLine("Press Enter to play");
-            Console.ReadLine(); 
-            // int countTotal = 100;
-           // int fizzNumber = 3;
-           // int buzzNumber = 5;
-            for (int i= 1; i <= countTotal; i++)
-            {
-                if ((i % fizzNumber) == 0 && (i % buzzNumber) == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if ((i % fizzNumber) == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if ((i % buzzNumber) == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            Console.ReadLine(); */
+            int countTotal = 100;
+            int fizzNumber = 3;
+            int buzzNumber = 5;
+            int bangNumber = 7;
+            int bongNumber = 11;
+             for (int i= 1; i <= countTotal; i++)
+             {
+                 string response = "";
+                 if ((i % fizzNumber) == 0)
+                 {
+                    response = "Fizz";
+                 }
+                 if ((i % buzzNumber) == 0)
+                 {
+                     response = response + "Buzz";
+                 }
+                 if ((i % bangNumber) == 0)
+                 {
+                     response = response + "Bang";
+                 }
+                 if ((i % bongNumber) == 0)
+                 {
+                     response = "Bong";
+                 }
+                 if (string.IsNullOrEmpty(response))
+                 {
+                     response = i.ToString();
+                 }
+                 Console.WriteLine(response);
+             }
         }
     }
 }
