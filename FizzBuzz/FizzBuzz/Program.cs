12431e2fe52e067c250e4c6b1fa16c4c5874ab3d
Espe-On <eljoe0@googlemail.com>
2020-02-03 14:29:46 +0000

    adding .git ignore

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countTotal = 100;
+            for (int i= 1; i <= countTotal; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
