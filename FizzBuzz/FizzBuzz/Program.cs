0178441a7f0ab2d6dc4b99ad8db9c0b6200d3be8
Espe-On <eljoe0@googlemail.com>
2020-02-03 15:02:27 +0000

    Added User Input options

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -6,12 +6,23 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            int countTotal = 100;
-            int fizzNumber = 3;
-            int buzzNumber = 5;
+            Console.WriteLine("We are going to play Fizz Buzz!");
+            Console.WriteLine("What Number is Fizz?");
+            int fizzNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What Number is Buzz?");
+            int buzzNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What Number are we going up to?");
+            int countTotal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fizz is " + fizzNumber + ", Buzz is " + buzzNumber);
+            Console.WriteLine("We are going up to " + countTotal);
+            Console.WriteLine("Press Enter to play");
+            Console.ReadLine(); 
+            // int countTotal = 100;
+           // int fizzNumber = 3;
+           // int buzzNumber = 5;
             for (int i= 1; i <= countTotal; i++)
             {
-                if ((i % fizzNumber*buzzNumber) == 0)
+                if ((i % fizzNumber) == 0 && (i % buzzNumber) == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
