a945c4257ae97fdcdd741603e4f80c8e62c4253f
Espe-On <eljoe0@googlemail.com>
2020-02-03 14:43:59 +0000

    Wrote original idea for FizzBuzz Program

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -7,9 +7,26 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             int countTotal = 100;
+            int fizzNumber = 3;
+            int buzzNumber = 5;
             for (int i= 1; i <= countTotal; i++)
             {
-                Console.WriteLine(i);
+                if ((i % fizzNumber*buzzNumber) == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ((i % fizzNumber) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((i % buzzNumber) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
